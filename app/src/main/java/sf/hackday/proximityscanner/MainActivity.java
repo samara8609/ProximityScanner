3f3c5c4815b3ac5c112fc1c72dc7a746aab9006d
Aaron Brucker <aaron.brucker@gmail.com>
2016-06-15 10:13:31 -0500

    fixing buildversions

--- app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
+++ app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
@@ -1,20 +1,18 @@
 package sf.hackday.proximityscanner;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.app.Activity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
