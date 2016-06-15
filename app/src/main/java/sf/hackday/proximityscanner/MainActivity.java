65b467725d2e91d3c6afaf385cbb54daf9d73bd0
Aaron Brucker <aaron.brucker@gmail.com>
2016-06-15 11:17:58 -0500

    updating

--- app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
+++ app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
@@ -1,18 +1,37 @@
 package sf.hackday.proximityscanner;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.widget.Toast;
+import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
+        if(null == nfcAdapter) {
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if(!nfcAdapter.isEnabled()) {
+
+        } else {
+
+        }
+
+        handleIntent(getIntent());
     }
 
     @Override
@@ -22,4 +41,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void handleIntent(Intent intent) {
+
+    }
 }
