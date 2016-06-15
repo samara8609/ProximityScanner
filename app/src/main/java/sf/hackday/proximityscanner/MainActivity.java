71acf7e61f191f2b08760135ac78bbc9e385e622
Aaron Brucker <aaron.brucker@gmail.com>
2016-06-15 13:49:32 -0500

    updating

--- app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
+++ app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
@@ -1,51 +1,33 @@
 package sf.hackday.proximityscanner;
 
 import android.app.PendingIntent;
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.widget.Toast;
-import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
-    private NfcAdapter nfcAdapter;
-    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, InsuranceCardActivity.class));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, VehicleCardActivity.class));
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        this.handleIntent(intent);
-    }
-
-
-    private void handleIntent(Intent intent) {
-
-    }
 }
---
+++ app/src/main/java/sf/hackday/proximityscanner/VehicleCardActivity.java
@@ -0,0 +1,17 @@
+package sf.hackday.proximityscanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by aaron on 6/15/16.
+ */
+
+public class VehicleCardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vehicle_card);
+    }
+}
