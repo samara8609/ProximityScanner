74f0a6d7e392fb5387ff16f248ca48e585a6631d
Aaron Brucker <aaron.brucker@gmail.com>
2016-06-15 13:52:31 -0500

    removing unused imports

--- app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
+++ app/src/main/java/sf/hackday/proximityscanner/MainActivity.java
@@ -1,12 +1,9 @@
 package sf.hackday.proximityscanner;
 
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
-import android.widget.Button;
 
 
 public class MainActivity extends Activity {
