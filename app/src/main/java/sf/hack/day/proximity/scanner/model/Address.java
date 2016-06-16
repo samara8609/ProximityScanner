dc8f569dbdd175b86be278077002f9754c3822c0
Aaron Brucker <aaron.brucker@gmail.com>
2016-06-15 21:21:12 -0500

    adding model classes

---
+++ app/src/main/java/sf/hack/day/proximity/scanner/model/Address.java
@@ -0,0 +1,18 @@
+package sf.hack.day.proximity.scanner.model;
+
+/**
+ * Created by aaron on 6/15/16.
+ */
+
+public class Address {
+
+    public String address1;
+
+    public String address2;
+
+    public String city;
+
+    public String state;
+
+    public String zip;
+}
---
+++ app/src/main/java/sf/hack/day/proximity/scanner/model/Policy.java
@@ -0,0 +1,18 @@
+package sf.hack.day.proximity.scanner.model;
+
+/**
+ * Created by aaron on 6/15/16.
+ */
+
+public class Policy {
+
+    public String id;
+
+    public String agent;
+
+    public String effectiveDate;
+
+    public String customerName;
+
+    public Address address;
+}
