72375807e32597134a01f58f324c4c6dd873102a
Aaron Brucker <aaron.brucker@gmail.com>
2016-06-16 00:57:06 -0500

    updating

---
+++ app/src/main/java/sf/hack/day/proximity/scanner/services/QuoteRetrieval.java
@@ -0,0 +1,17 @@
+package sf.hack.day.proximity.scanner.services;
+
+import sf.hack.day.proximity.scanner.model.Quote;
+/**
+ * Created by aaron on 6/16/16.
+ */
+
+public final class QuoteRetrieval {
+
+    private QuoteRetrieval(){}
+
+    public static double retrieve(String UUID, String coverage) {
+        return 0.00;
+
+    }
+
+}
