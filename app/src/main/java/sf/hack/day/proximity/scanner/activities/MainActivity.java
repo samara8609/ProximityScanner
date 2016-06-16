0deb6c778d2d4c15722dd37fe634e7ba5740ea53
Aaron Brucker <aaron.brucker@gmail.com>
2016-06-15 19:19:38 -0500

    updating

--- app/src/main/java/sf/hack/day/proximity/scanner/activities/MainActivity.java
+++ app/src/main/java/sf/hack/day/proximity/scanner/activities/MainActivity.java
@@ -1,11 +1,10 @@
-package sf.hackday.scanner.activities;
+package sf.hack.day.proximity.scanner.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
-
 
 public class MainActivity extends Activity {
 
